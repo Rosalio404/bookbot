5ce47d45b8a704166fa3267cd79a91e7d53c0613
Rosalio404 <Rosalio404@protonmail.com>
2024-06-27 19:04:17 -0500

    Add word count

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 def main():
-        with open("./books/frankenstein.txt") as f:
-            file_contents = f.read()
+    # use with to open a file
+    with open("./books/frankenstein.txt") as f:
+        file_contents = f.read()
+        words = file_contents.split()
+        word_count = 0
+        for word in words :
+            word_count += 1
+        print(word_count)
 
 main()
