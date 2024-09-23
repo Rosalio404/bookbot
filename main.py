59f89ac36949bfe1ffac10c879a91ede703bb80e
Rosalio404 <Rosalio404@protonmail.com>
2024-09-23 06:33:27 -0500

    Refactor to separate functions

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    print(f"{num_words} words found in the document")
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+def get_book_text(path):
     # use with to open a file
-    with open("./books/frankenstein.txt") as f:
-        file_contents = f.read()
-        words = file_contents.split()
-        word_count = 0
-        for word in words :
-            word_count += 1
-        print(word_count)
+    with open(path) as f:
+        return f.read()
 
 main()
