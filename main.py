cacabe54e5fc829a605c1a86e6cebc02385cafe0
Rosalio404 <Rosalio404@protonmail.com>
2024-09-24 06:49:54 -0500

    Add function to get number of each character

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
+    num_chars = get_num_chars(text)
     print(f"{num_words} words found in the document")
+    print(num_chars)
 
 def get_num_words(text):
     words = text.split()
@@ -12,5 +14,13 @@ def get_book_text(path):
     # use with to open a file
     with open(path) as f:
         return f.read()
+    
+def get_num_chars(text):
+    chars = list(text.lower())
+    num_chars = dict.fromkeys(chars, 0)
+    for char in chars:
+        num_chars[char] += 1
+    return num_chars
+
 
 main()
